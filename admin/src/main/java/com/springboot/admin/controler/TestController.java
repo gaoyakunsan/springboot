378099affc4d7d7fc84bb264e504132ae48bf1fc
yakungao <yakungao@pptv.com>
2018-03-07 18:16:04 +0800

    更改.gitignore文件

--- admin/src/main/java/com/springboot/admin/controler/TestController.java
+++ admin/src/main/java/com/springboot/admin/controler/TestController.java
@@ -34,6 +34,7 @@ public class TestController {
     public String Test() {
         //List<AdmainDO> list = admainService.findAllAdmain();
         System.out.println("dsafdsf");
+        System.out.println(1111);
         return "index";
     }
 
