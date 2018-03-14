97692ed9c169668d676a458dbafa3e4e82faf28f
yakungao <yakungao@pptv.com>
2018-03-14 14:21:29 +0800

    提交建表sql

---
+++ core/src/main/resources/projectresource/spring_boot.sql
@@ -0,0 +1,104 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.21-log : Database - spring_boot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`spring_boot` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `spring_boot`;
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL COMMENT '父id',
+  `name` varchar(45) DEFAULT NULL,
+  `url` varchar(70) DEFAULT NULL,
+  `sort` int(11) NOT NULL DEFAULT '1',
+  `is_menu` tinyint(1) NOT NULL DEFAULT '0',
+  `is_enable` tinyint(1) NOT NULL DEFAULT '0',
+  `icon` varchar(45) DEFAULT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modify_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_del` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_permission` */
+
+/*Table structure for table `sys_re_role_permission` */
+
+DROP TABLE IF EXISTS `sys_re_role_permission`;
+
+CREATE TABLE `sys_re_role_permission` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_re_role_permission` */
+
+/*Table structure for table `sys_re_user_role` */
+
+DROP TABLE IF EXISTS `sys_re_user_role`;
+
+CREATE TABLE `sys_re_user_role` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_re_user_role` */
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL COMMENT '用户id',
+  `name` varchar(45) DEFAULT NULL COMMENT '用户名称',
+  `sort` int(11) NOT NULL DEFAULT '1' COMMENT '排序',
+  `description` varchar(200) DEFAULT NULL COMMENT '描述',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modify_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_del` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role` */
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL COMMENT '用户id',
+  `username` varchar(45) DEFAULT NULL COMMENT '用户名称',
+  `password` varchar(45) DEFAULT NULL COMMENT '用户密码',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modify_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_del` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`password`,`create_time`,`last_modify_time`,`is_del`) values (1,'2','2','2018-03-14 11:22:47','2018-03-14 11:22:47',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
